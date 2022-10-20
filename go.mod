80af137fe3898cf00a4810b5c1c6a709fe0d700a
Leonel Sanches <113376080+seg-leonelsanches@users.noreply.github.com>
2022-10-20 16:42:29 -0700

    Segment initialization, closure and server graceful shutdown.

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.19
 
 require (
 	github.com/gin-gonic/gin v1.8.1
+	github.com/joho/godotenv v1.4.0
 	github.com/segmentio/analytics-go v3.1.0+incompatible
 )
 
--- main.go
+++ main.go
@@ -1,13 +1,28 @@
 package main
 
 import (
+	"log"
+	"net/http"
 	"os"
+	"os/signal"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"github.com/segmentio/analytics-go"
 )
 
 var client analytics.Client
+
+func init() {
+
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	client = analytics.New(os.Getenv("SEGMENT_WRITE_KEY"))
+}
 
 func main() {
 	router := gin.Default()
@@ -15,8 +30,30 @@ func main() {
 	router.GET("/keyboards/:id", getKeyboardByID)
 	router.POST("/keyboards", postKeyboards)
 
-	router.Run(":3001")
+	server := &http.Server{
+		Addr:    ":3001",
+		Handler: router,
+	}
 
-	client := analytics.New(os.Getenv("SEGMENT_WRITE_KEY"))
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+
+	go func() {
+		<-quit
+		log.Println("receive interrupt signal")
+		if err := server.Close(); err != nil {
+			log.Fatal("Server Close:", err)
+		}
+	}()
+
+	if err := server.ListenAndServe(); err != nil {
+		if err == http.ErrServerClosed {
+			log.Println("Server closed under request")
+		} else {
+			log.Fatal("Server closed unexpect")
+		}
+	}
+
+	log.Println("Server exiting")
 	defer client.Close()
 }
