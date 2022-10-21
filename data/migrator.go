884e9b939a14aa1b9cf1ee68e596d2c3c30a355a
Leonel Sanches <113376080+seg-leonelsanches@users.noreply.github.com>
2022-10-21 15:24:49 -0700

    Creating a Migration State entity to avoid double seed.

--- data/migrator.go
+++ data/migrator.go
@@ -8,16 +8,26 @@ func Migrate() {
 	// Migrate the schema
 	Db.AutoMigrate(&models.Keyboard{})
 	Db.AutoMigrate(&models.Manufacturer{})
+	Db.AutoMigrate(&models.MigrationState{})
 
 	// Create
-	Db.Create([]models.Keyboard{
-		{Id: "1", Model: "Williams Allegro III", Manufacturer: "Williams", Price: 349.99},
-		{Id: "2", Model: "Yamaha P-125", Manufacturer: "Yamaha", Price: 699.99},
-		{Id: "3", Model: "Casio CDP-S100", Manufacturer: "Casio", Price: 449.99},
-	})
-	Db.Create([]models.Manufacturer{
-		{Id: "1", Name: "Williams"},
-		{Id: "2", Name: "Yamaha"},
-		{Id: "3", Name: "Casio"},
-	})
+	var migrationState models.MigrationState
+	result := Db.First(&migrationState, "name = ?", "Initial")
+	if result.RowsAffected == 0 {
+		Db.Create(&models.MigrationState{
+			Id:   "1",
+			Name: "Initial",
+		})
+		Db.Create([]models.Keyboard{
+			{Id: "1", Model: "Williams Allegro III", Manufacturer: "Williams", Price: 349.99},
+			{Id: "2", Model: "Yamaha P-125", Manufacturer: "Yamaha", Price: 699.99},
+			{Id: "3", Model: "Casio CDP-S100", Manufacturer: "Casio", Price: 449.99},
+		})
+		Db.Create([]models.Manufacturer{
+			{Id: "1", Name: "Williams"},
+			{Id: "2", Name: "Yamaha"},
+			{Id: "3", Name: "Casio"},
+		})
+	}
+
 }
